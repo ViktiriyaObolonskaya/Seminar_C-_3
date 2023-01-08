158746de3b07462dc65e2438dd11d5386bfc3f04
Viktoria Obolonskaya <obolonskaya_93@mail.ru>
2023-01-09 00:26:40 +0300

    ещё раз2)

---
+++ Zadachi_3/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 1. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Проверка на пятизначность обязательна
+
+// Console.WriteLine("Введите пятизначное число:");
+// int num = int.Parse (Console.ReadLine()!);
+// string Number = num.ToString();
+// if (Number.Length == 5){
+// if ((Number[0] == Number[4]) && (Number[1] == Number[3] )){
+//     Console.WriteLine($"Число {num} является палиндромом");
+// }
+// else
+//     Console.WriteLine($"Число {num} не является палиндромом");
+// }
+
+//___________________________________________________________________________
+
+
+
+// Задача 2. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// Console.WriteLine("Введите координату Х для первой точки:");
+// int X1 = int.Parse (Console.ReadLine()!);
+// Console.WriteLine("Введите координату Y для первой точки:");
+// int Y1 = int.Parse (Console.ReadLine()!);
+// Console.WriteLine("Введите координату Z для первой точки:");
+// int Z1 = int.Parse (Console.ReadLine()!);
+
+// Console.WriteLine("Введите координату Х для второй точки:");
+// int X2 = int.Parse (Console.ReadLine()!);
+// Console.WriteLine("Введите координату Y для второй точки:");
+// int Y2 = int.Parse (Console.ReadLine()!);
+// Console.WriteLine("Введите координату Z для второй точки:");
+// int Z2 = int.Parse (Console.ReadLine()!);
+
+// double rastoaynie = Math.Sqrt(Math.Pow (X2-X1,2) + Math.Pow (Y2-Y1,2) + Math.Pow (Z2-Z1,2));
+// Console.WriteLine($"Расстояние между точками {rastoaynie:f1} ");
+
+//___________________________________________________________________________
+
+
+// Задача 3. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// Console.WriteLine("Введите число");
+// int N = int.Parse (Console.ReadLine()!);
+// for(int i = 1; i<=N; i++){
+//     Console.Write($"{Math.Pow(i, 3)} ");
+// }
+
+//___________________________________________________________________________
